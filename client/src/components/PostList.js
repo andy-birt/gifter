09aac202cc162a605e0b838da890f8024cae1748
Andrew Birt <abirt1986@gmail.com>
2022-03-11 20:34:12 -0500

    create post component and include it in the list component for post

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { PostContext } from "../providers/PostProvider";
+import Post from "./Post";
 
 const PostList = () => {
   const { posts, getAllPosts } = useContext(PostContext);
@@ -11,13 +12,7 @@ const PostList = () => {
   return (
     <div>
       {posts.map((post) => (
-        <div key={post.id}>
-          <img src={post.imageUrl} alt={post.title} />
-          <p>
-            <strong>{post.title}</strong>
-          </p>
-          <p>{post.caption}</p>
-        </div>
+        <Post key={post.id} post={post} />
       ))}
     </div>
   );
