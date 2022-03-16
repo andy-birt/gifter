f621f6a01aa574c4132f541d326655bc25cd558b
Andrew Birt <abirt1986@gmail.com>
2022-03-16 11:52:47 -0400

    create like controller for adding likes to post

---
+++ Gifter/Controllers/LikeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Gifter.Repositories;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikeController : ControllerBase
+    {
+        private readonly ILikeRepository _likeRepo;
+
+        public LikeController(ILikeRepository likeRepo)
+        {
+            _likeRepo = likeRepo;
+        }
+
+        [HttpPost]
+        public IActionResult Post(int postId)
+        {
+            _likeRepo.Add(postId);
+            return NoContent();
+        }
+    }
+}
