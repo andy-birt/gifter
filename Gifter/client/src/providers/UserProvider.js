bf3a90887bafbf5a9aeabe889637458c0c17b2f9
Andrew Birt <abirt1986@gmail.com>
2022-03-17 20:12:23 -0400

    create currentUser state with a default value and implement useNavigate

--- Gifter/client/src/providers/UserProvider.js
+++ Gifter/client/src/providers/UserProvider.js
@@ -1,31 +1,28 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext();
 
 export const UserProvider = (props) => {
 
+  //* Get the gifterUser value stored in localStorage
+  const currentUserInLocalStorage = JSON.parse(localStorage.getItem("gifterUser"));
+
   //* This state controls the user that currentUser interacts with
   const [user, setUser] = useState();
 
-  // const [currentUser, setCurrentUser] = useState({});
+  //* State that uses currently logged in user via localStorage
+  const [currentUser, setCurrentUser] = useState(currentUserInLocalStorage);
 
-  // useEffect(() => {
-  //   const stringifiedUser = localStorage.getItem("gifterUser");
-  //   const currentUser = JSON.parse(stringifiedUser);
-    
-  //   getUserByEmail(currentUser.email).then(setCurrentUser);
-    
-  //   return () => {
-  //     setCurrentUser({});
-  //   }
-  // }, []);
+  const navigate = useNavigate();
 
   const login = (userObject) => {
     fetch(`api/userprofile/getbyemail?email=${userObject.email}`)
       .then((r) => r.json())
       .then((userObjFromDB) => {
         localStorage.setItem("gifterUser", JSON.stringify(userObjFromDB));
-        
+        setCurrentUser(currentUserInLocalStorage);
+        navigate('/');
       })
   };
 
@@ -40,21 +37,17 @@ export const UserProvider = (props) => {
       .then((response) => response.json())
       .then((userObject) => {
         localStorage.setItem("gifterUser", JSON.stringify(userObject));
-        
+        setCurrentUser(currentUserInLocalStorage);
+        navigate('/');
       });
   };
-
-  // const getUserByEmail = (email) => {
-  //   return fetch(`api/userprofile/getbyemail?email=${email}`)
-  //           .then((r) => r.json());
-  // }
 
   const logout = () => {
     localStorage.clear();
   };
 
   return (
-    <UserContext.Provider value={{ login, register, logout, setUser, user }} >
+    <UserContext.Provider value={{ currentUser, login, register, logout, setUser, user }} >
       {props.children}
     </UserContext.Provider>
   );
